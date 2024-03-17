2c5564aeecbb07b323506ddc395907920fe2007a
Sandhiya94 <sandhiyanatraj@gmail.com>
2024-03-17 15:38:46 -0400

    commit 8

--- src/main/java/com/task/kaiburrassessment/KaiburrassessmentApplication.java
+++ src/main/java/com/task/kaiburrassessment/KaiburrassessmentApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KaiburrassessmentApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(KaiburrassessmentApplication.class, args);
 	}
